595735c89b8e25c0c565dc412c5f66c311d798b9
prabh <prabhnoorsingh23000@gmail.com>
2023-06-27 15:25:00 -0400

    server.js chanaged

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+//hi there sample git commit
 
 const app = express();
 
 app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 
-app.use(cors());
+hi there
