8196d3fab0aba53c144be740fe38e6324f5e2efe
prabh <prabhnoorsingh23000@gmail.com>
2023-06-27 19:18:20 -0400

    Added connection string

--- server/index.js
+++ server/index.js
@@ -10,3 +10,15 @@ app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://prabh:PrabhnooR2300@cluster0.kavj6sa.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(()=> app.listen(PORT, ()=>
+    console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err)=> console.log(err.message));
+
+    // mongoose.set('useFindAndModify', false);
