05d3bec4f92247ba89f3e6ff61373e4289686afd
prabh <prabhnoorsingh23000@gmail.com>
2023-07-12 09:59:37 -0400

    crud 1.1

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -8,6 +8,16 @@ export const getStudents = async (req,res)=> {
     }
 }
 
-export const createStudent = (req,res)=> {
-    res.send('Router is working');
+export const createStudent = async(req,res)=> {
+    const student = req.body;
+    
+    const newStudent = new student(student);
+                           //model //variable
+
+    try {
+        await newStudent.save();
+        res.status(201).json(newStudent);
+    } catch (error) {
+        res.status(409).json({ message:error.message });
+    }
 }
