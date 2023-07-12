d4d1624cb8637c493070896b9d906279519329f2
prabh <prabhnoorsingh23000@gmail.com>
2023-07-11 20:50:29 -0400

    Setting up routes 1

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import studentRoutes from './routes/student.js';
 //hi there sample git commit
-
 const app = express();
+app.use('students', studentRoutes);
+
 
 app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
@@ -18,8 +20,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true, useUnifiedTopology:true
 }).then(()=> app.listen(PORT, ()=>
-    console.log(`Connection is established and running on port: ${PORT}`)
-    
+    console.log(`Connection is established and running on port: ${PORT}`)   
 )).catch((err)=> console.log(err.message));
 
-    // mongoose.set('useFindAndModify', false);
+    // mongoose.set('useFindAndModify', false); 
