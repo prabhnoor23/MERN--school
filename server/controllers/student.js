607ce178165f95ae22e1032c9e55c1466ae198e9
prabh <prabhnoorsingh23000@gmail.com>
2023-07-12 09:43:13 -0400

    crud 1.0

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,3 +1,13 @@
-export const getStudents = (req,res)=> {
+export const getStudents = async (req,res)=> {
+    try {
+        const allStudents = await  student.find();
+
+        res.status(200).json(allStudents);
+    } catch (error) {
+        res.status(404).json({ message: error.message});
+    }
+}
+
+export const createStudent = (req,res)=> {
     res.send('Router is working');
 }
