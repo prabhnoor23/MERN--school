d503cf302bcc062624189c6a99e58fdcd6e544da
prabh <prabhnoorsingh23000@gmail.com>
2023-07-14 17:19:28 -0400

    components 1.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';  
 
 function App() {
   return (
+    <div className = "App">
+
+      <Container maxWidth = "lg">
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant='h2' align = 'center'> Student Create and Show </Typography>
+        </AppBar>
     
+      </Container>
+
+    </div>
   );
 }
 
