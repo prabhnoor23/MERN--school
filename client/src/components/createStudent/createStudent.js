f6a2429e2793be9a62f5b31fce7a4bb9c2904b51
prabh <prabhnoorsingh23000@gmail.com>
2023-07-21 19:10:23 -0400

    components 3.3 createStudents

--- client/src/components/createStudent/createStudent.js
+++ client/src/components/createStudent/createStudent.js
@@ -1,34 +1,52 @@
 //create students
 
-import * as React from 'react';
+import React, {useState} from 'react';
 import Box from '@material-ui/core/Box';
+
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import useStyles from '../../styles.js';
 
-export default function BasicTextFields() {
-   const classes = useStyles();
-    return (
-       <> <h2> Create Student </h2>
-    <Box
-    className={classes.root}
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-        
-        
-      <TextField id="outlined-basic" label="Registration No." variant="outlined" />
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Grade" variant="outlined" />
-      <TextField id="outlined-basic" label="Section" variant="outlined" />
+import { makeStyles } from '@material-ui/core';
 
-      <Button variant="contained" color='primary'>Create</Button>
-      
-    </Box>
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': { margin: theme.spacing(1), width: '25ch', },
+
+  },
+}));
+export default function CreateStudent() {
+  const classes = useStyles();
+
+  const [student, setStudent] = React.useState({
+
+    regNo: 0,
+    studentName: '',
+    grade: '',
+    section: ''
+  });
+
+  return (
+    <> <h2> Create Student </h2>
+      <Box
+        className={classes.root}
+        // component="form"
+        // sx={{
+        //   '& > :*': { margin: theme.spacing(1), width: '25ch' },
+        // }}
+        noValidate
+        autoComplete="off"
+      >
+
+
+        <TextField id="outlined-basic" label="Registration No." variant="outlined" value={student.regNo}/>
+        <TextField id="outlined-basic" label="Name" variant="outlined" value={student.studentName}/>
+        <TextField id="outlined-basic" label="Grade" variant="outlined" value={student.grade}/>
+        <TextField id="outlined-basic" label="Section" variant="outlined" value={student.section}/>
+
+        <Button variant="contained" color='primary'>Create</Button>
+
+      </Box>
     </>
   );
 }
