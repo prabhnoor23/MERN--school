c3e9708dda720df07c434938ee40524b985228e9
prabh <prabhnoorsingh23000@gmail.com>
2023-08-10 16:58:21 -0400

    Show Students and Delete Functionality 1.0

--- client/src/components/showStudent/showStudent.js
+++ client/src/components/showStudent/showStudent.js
@@ -1,5 +1,5 @@
 //show students
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,56 +7,66 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+
+
+export default function ShowStudent() {
+
+  const [studentsList, setStudentList] = useState([]);
+
+  const deleteStudent = (id) =>{
+    axios.delete(`https://localhost:5000/students/${id}`).then( () =>{
+      window.location.reload(false);
+    })
   }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-  
-  export default function BasicTable() {
-    
-    // const[studentsList, setStudentList] = useState([])
-    
-    return (
 
-      <>
+  useEffect(() => {
+    axios.get('http://localhost:5000/students').then((allStudents) => { //".then" is start of promise function
+      setStudentList(allStudents.data);
+    })
+  }, [])
+
+  return (
+
+    <>
       <h2>ALL Students</h2>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Registration Number</TableCell>
+              <TableCell align="right">Grade</TableCell>
+              <TableCell align="right">Seciton</TableCell>
+              <TableCell align="right">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {studentsList.map((student, key) => (
               <TableRow
-                key={row.name}
+                key={key}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {student.studentName}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{student.regNo}</TableCell>
+                <TableCell align="right">{student.grade}</TableCell>
+                <TableCell align="right">{student.section}</TableCell>
+                <TableCell align="right">
+                  <IconButton aria-label="delete" size="small" onClick={() => deleteStudent(student._id)}>
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
+
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      </>
-    );
-  }
+    </>
+  );
+}
